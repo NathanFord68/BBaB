d5047c7415df9f912a989305b973d573aca20864
Nathan Ford <NathanFord68@gmail.com>
2020-04-09 18:38:52 -0600

    Added connection string to new db

--- BBaB/Service/Data/Connections/ConnectionData.cs
+++ BBaB/Service/Data/Connections/ConnectionData.cs
@@ -14,7 +14,8 @@ namespace BBaB.Service.Data
         public ConnectionData()
         {
             //this._sqlConnectionString = "Data Source=DESKTOP-S1TTCNK;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; //Nathan's Laptop
-            this._sqlConnectionString = "Server=localhost;Database=master;Trusted_Connection=True;";    //Nathan's desktop computer
+            //this._sqlConnectionString = "Server=localhost;Database=master;Trusted_Connection=True;";    //Nathan's desktop computer
+            this._sqlConnectionString = "Server=bbabdb.cejy5o3ljc1k.us-east-2.rds.amazonaws.com;Database=bbab;User ID=BBaBAdmin;Password=PlY%e$O8$bsNiyMc5WS;";
         }
 
         public SqlConnection GetConnection()
